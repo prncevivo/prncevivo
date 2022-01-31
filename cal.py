bfd4e8f3df54c9ee1ee7cc4b31d4316421232e9f
prncevivo <98744368+prncevivo@users.noreply.github.com>
2022-01-31 16:34:24 +0530

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,20 @@
+first = input("Enter the first number: ")
+operator = input("Enter the operator (+,-,*,/,%) : ")
+second = input("Enter the second number: ")
+
+first = int(first)
+
+second = int(second)
+
+if operator == "+":
+    print(first+second)
+elif operator == "-":
+    print(first-second)
+elif operator == "*":
+    print(first*second)
+elif operator == "/":
+    print(first/second)
+elif operator == "%":
+    print(first%second)
+else:
+    print("Invalid operator")
